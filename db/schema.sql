27e945d09d1495001175b05631c024ef1cdf085c
Nicole Stark <nicolestark@Nicoles-iMac.attlocal.net>
2023-08-17 20:48:41 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,17 +6,6 @@ CREATE DATABASE pokemon_db;
 -- use database
 USE pokemon_db;
 
--- Pokemon table
-CREATE TABLE pokemon (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    type VARCHAR(30) NOT NULL,
-    moves INT,
-    is_evolved BOOLEAN DEFAULT false,
-    trainer_id INT,
-    PRIMARY KEY (id)
-);
-
 -- Moves table
 CREATE TABLE moves (
     id INT NOT NULL AUTO_INCREMENT,
@@ -33,4 +22,17 @@ CREATE TABLE trainers (
     num_badges INT DEFAULT false,
     date_added TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
+);
+
+-- Pokemon table
+CREATE TABLE pokemon (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(30) NOT NULL,
+    moves INT,
+    is_evolved BOOLEAN DEFAULT false,
+    trainer_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (moves) REFERENCES moves(id),
+    FOREIGN KEY (trainer_id) REFERENCES trainers(id)
 );
